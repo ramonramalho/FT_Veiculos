1faed5ab1f1c1e6ec57db40458055aa676e54425
sergiodeveloper <sergio-fh@hotmail.com>
2017-05-04 15:05:34 -0300

    Formatando construtor de ConcessionariaModerna

--- src/ConcessionariaModerna.cpp
+++ src/ConcessionariaModerna.cpp
@@ -11,14 +11,15 @@
 
 using namespace std;
 
-ConcessionariaModerna::ConcessionariaModerna(string site, string facebook, string tweeter, string whatsapp,
-        string nome, string endereco, string cidade, string estado, string telefone) : Concessionaria(nome, endereco, cidade, estado, telefone) {
-
+ConcessionariaModerna::ConcessionariaModerna(
+  string site, string facebook, string tweeter, string whatsapp,
+  string nome, string endereco, string cidade, string estado, string telefone
+) : Concessionaria(nome, endereco, cidade, estado, telefone)
+{
     this->site = site;
     this->facebook = facebook;
     this->tweeter = tweeter;
     this->whatsapp = whatsapp;
-
 };
 
 const string ConcessionariaModerna::getSite() {
